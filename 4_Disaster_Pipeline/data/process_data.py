328882386b7dff057b0f98518b55fcffb7985227
Sakchhi Srivastava <sakchhi.sri@gmail.com>
2020-04-05 08:55:09 +0530

    feat: ETL Pipeline
    Read messages and categories, clean and merge them and write to database

--- 4_Disaster_Pipeline/data/process_data.py
+++ 4_Disaster_Pipeline/data/process_data.py
@@ -1,16 +1,52 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load data from csv files at given filepath
+    :param messages_filepath: Filepath for messages csv file
+    :param categories_filepath: Filepath for categories csv file
+    :return: Dataframe merging both the csv files
+    """
+    df_categories = pd.read_csv(categories_filepath)
+    df_messages = pd.read_csv(messages_filepath)
+    df = pd.merge(df_categories, df_messages, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Cleans Dataframe by converting Response categories into separate columns with binary values
+    :param df: Dataframe containing responses and categories
+    :return: Cleaned Dataframe
+    """
+    categories = df.categories.str.split(';', expand=True)
+
+    row = categories.iloc[0, :]
+    category_columns = row.apply(lambda r: r.split('-')[0])
+    categories.columns = category_columns
+
+    for column in categories:
+        categories[column] = categories[column].apply(lambda r: r.split('-')[1].strip())
+        categories[column] = categories[column].astype(int)
+
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.merge(df, categories, left_index=True, right_index=True)
+
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save dataframe to sqlite database at specified path
+    :param df: Dataframe to be saved to sqlite database
+    :param database_filename: Path and name of database to save dataframe to
+    :return: None
+    """
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('disaster_response_etl', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -27,6 +63,7 @@ def main():
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
+        print(df.head())
         
         print('Cleaned data saved to database!')
     
